fc16fd2c961ced389e206a96f02913a77a1e760b
Will Boyce <me@willboyce.com>
2015-06-11 12:17:25 +0100

    hunt for config in parent directories

--- lib/app_config.rb
+++ lib/app_config.rb
@@ -2,39 +2,47 @@ require "rubygems"
 
 class AppConfig
   FILENAME = ".pt.yaml"
-  
+
   def self.load
     require "yaml"
 
     # Read YAML config or quit.
+    path = Dir.pwd
     config = nil
-    begin
-      config = YAML.load_file FILENAME
-    rescue Exception => e
+    while config.nil?
+      candidate = File.join(path, FILENAME)
+      if not File.exists?(candidate)
+        path = File.absolute_path(File.join(path, '..'))
+      else
+        config = YAML.load_file(candidate)
+      end
+      break if path == File.absolute_path(File.join(path, '..'))
+    end
+    if config.nil?
       raise "No #{FILENAME} file found in current directory; please create one."
     end
-    
+
     AppConfig.new :config => config
   end
-  
+
   def initialize opts={:config => Hash.new}
     @config = opts[:config]
-    
+
     raise "Config must include \"project\" key" if not @config.has_key? "project"
     raise "Config must include \"api_key\" key" if not @config.has_key? "api_key"
     raise "Config must include \"user\" key" if not @config.has_key? "user"
   end
-  
+
   def project
     @config["project"]
   end
-  
+
   def api_key
     @config["api_key"]
   end
-  
+
   def user
     @config["user"]
   end
-  
+
 end
